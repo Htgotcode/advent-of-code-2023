4974170dda4a423266cd9faaea0cb352f7f75f3e
Kyle van Antwerpen <kyle.vanantwerpen@gmail.com>
2023-12-12 18:57:31 +0200

    attempt 2

--- day-1/day-1.py
+++ day-1/day-1.py
@@ -4,37 +4,52 @@ import re
 with open('day-1\input.txt') as f:
     lines = f.readlines()
 
-list = []
-concatList = []
-sum =0
-for line in lines:
-    list.append(re.findall("[0-9]",line))
+# list = []
+# concatList = []
+# sum =0
+# for line in lines:
+#     list.append(re.findall("[0-9]",line))
 
-for i in range(0,1000):
-    concatList.append(list[i][0] + list[i][-1])
-    sum += int(concatList[i])
-
-print(sum)
+# for i in range(0,1000):
+#     concatList.append(list[i][0] + list[i][-1])
+#     sum += int(concatList[i])
 # P2
 list = []
 concatList = []
+replaceList=[]
 sum =0
 
-for line in lines:
-    line = re.sub("one", "1", line)
-    line = re.sub("two", "2", line)
-    line = re.sub("three", "3", line)
-    line = re.sub("four", "4", line)
-    line = re.sub("five", "5", line)
-    line = re.sub("six", "6", line)
-    line = re.sub("seven", "7", line)
-    line = re.sub("eight", "8", line)
-    line = re.sub("nine","9", line)
-    list.append(re.findall("[0-9]", line))
+numbersToReplace = {"one": "1","two": "2","three": "3","four": "4","five": "5","six": "6","seven": "7","eight": "8","nine": "9"}
+span = []
+index= 0
 
-for i in range(0,1000):
-    concatList.append(list[i][0] + list[i][-1])
-    print(concatList)
-    sum += int(concatList[i])
-print(sum)
+for key in numbersToReplace.keys():
+    index= 0
+    charToReplace = numbersToReplace[key]
+    for line in lines:
+        index += 1
+        if key in line:
+            search = re.search(key, line)
+            span.append([index, charToReplace, search.span()[0], search.span()[1]])
 
+
+for i in range(len(lines)):
+    for j in range(len(span)):
+        if span[j][0] == i + 1 :
+            newLine = line[:span[j][2]] + "" +str(span[j][1])+ "" + line[span[j][2]:]
+            print(newLine)
+        
+    # print(newLine)
+# for i in range(len(span)):
+#     replaceList.append(line[:span[i][2]] + " " + line[span[i][2]:])
+
+# print(replaceList)
+
+# for line in lines:
+    
+#     list.append(re.findall("[0-9]", line))
+
+# for i in range(0,1000):
+#     concatList.append(list[i][0] + list[i][-1])
+#     sum += int(concatList[i])
+# print(sum)
